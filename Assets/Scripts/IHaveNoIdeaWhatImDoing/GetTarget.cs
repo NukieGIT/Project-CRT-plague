9f0a21a843a26e14a259b9ad3907d82f4b789f2a
PoroCannon <jan1zachariasz@gmail.com>
2023-04-30 10:49:41 +0200

    objectHolder
    added objectHolder following camera rotation

--- Assets/Scripts/IHaveNoIdeaWhatImDoing/GetTarget.cs
+++ Assets/Scripts/IHaveNoIdeaWhatImDoing/GetTarget.cs
@@ -1,9 +1,22 @@
+using Movement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GetTarget : MonoBehaviour
 {
+    //objectHolder variables
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject objectHolder;
+    [SerializeField] private GameObject playerCamera;
+    private Quaternion _cameraRotation;
+    private Vector3 _F2 = Vector3.forward*2;
+    private IPlayerMovement _movement;
+
+    private void Awake()
+    {
+        _movement = GetComponent<IPlayerMovement>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,10 @@ public class GetTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _cameraRotation = playerCamera.transform.rotation;
+        var pPosition = _movement.Position;
+        objectHolder.transform.rotation = _cameraRotation;
+        objectHolder.transform.position = _movement.Position;
+        objectHolder.transform.position += objectHolder.transform.TransformDirection(Vector3.forward) * 2f; 
     }
 }
